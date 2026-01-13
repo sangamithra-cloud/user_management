e0ba3ad955c651ac1add26918134b38bc72ee103
R <sangamithra@uniqnex360.com>
2026-01-13 18:47:44 +0530

    elasticemail

--- user_management/settings.py
+++ user_management/settings.py
@@ -96,16 +96,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # needed for collectstatic 
 # CUSTOM USER MODEL
 AUTH_USER_MODEL = 'userapp.User'
 
-# EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.elasticemail.com'
+EMAIL_PORT = 2525  # or 587 for TLS
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'default@example.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = 'noreply@uniqnex360.com'
+
 
 
 # LOGIN/LOGOUT REDIRECTS
